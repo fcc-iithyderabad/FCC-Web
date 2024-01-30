9c2646b7b6f13ce08939dc748a4081692e4be7e7
anandk3012 <anandkash99@gmail.com>
2024-01-30 21:31:51 +0530

    cards

--- script.js
+++ script.js
@@ -2,24 +2,24 @@
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.from(".titlecard", {
-    duration: 1,
+    duration: 0.7,
     y: "40",
     opacity: 0
 })
 gsap.from(".horizon-card", {
-    duration: 1,
+    duration: 0.7,
     y: "40",
     opacity: 0
 })
 gsap.from(".quote-span", {
-    duration: 1,
+    duration: 0.7,
     y: "40",
     opacity: 0,
     stagger: 0.5,
     scrollTrigger: '.quote'
 })
 gsap.from(".arrow", {
-    duration: 1,
+    duration: 0.7,
     x: "-60",
     y: "60",
     opacity: 0,
