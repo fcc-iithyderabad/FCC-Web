2cb3723f30884ec4b426fd91b62177e55f25964d
anandk3012 <anandkash99@gmail.com>
2024-02-06 18:44:17 +0530

    sponsors updated

---
+++ pages-js/sponsors.js
@@ -0,0 +1,57 @@
+//   SPONSORS LINKS
+$(".card-1 img").on('click', function(){
+    window.open('https://www.abhibus.com' )
+    return false;
+})
+$(".card-2 img").on('click', function(){
+    window.open('https://www.decathlon.in' )
+    return false;
+})
+$(".card-3 img").on('click', function(){
+    window.open('https://www.stockgro.club' )
+    return false;
+})
+$(".card-4 img").on('click', function(){
+    window.open('https://www.finlatics.com' )
+    return false;
+})
+$(".card-5 img").on('click', function(){
+    window.open('https://www.monsterenergy.com/en-in/')
+    return false;
+})
+$(".card-6 img").on('click', function(){
+    window.open('https://www.theproductfolks.com')
+    return false;
+})
+$(".card-7 img").on('click', function(){
+    window.open('https://sidsfarm.com')
+    return false;
+})
+$(".card-8 img").on('click', function(){
+    window.open('https://www.grabon.in' )
+    return false;
+})
+$(".card-9 img").on('click', function(){
+    window.open('https://plumgoodness.com')
+    return false;
+})
+$(".card-10 img").on('click', function(){
+    window.open('https://startupnews.fyi')
+    return false;
+})
+$(".card-11 img").on('click', function(){
+    window.open('https://www.edufabrica.net')
+    return false;
+})
+$(".card-12 img").on('click', function(){
+    window.open('https://www.easemytrip.com')
+    return false;
+})
+$(".card-13 img").on('click', function(){
+    window.open('http://elearnmarkets.com')
+    return false;
+})
+$(".card-14 img").on('click', function(){
+    window.open('http://preplounge.com/en')
+    return false;
+})
