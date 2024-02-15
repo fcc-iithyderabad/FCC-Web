9923c7f1cb7228791a2236f13e4d71a6295aba07
anandk3012 <anandkash99@gmail.com>
2024-02-16 03:34:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,38 +70,38 @@ gsap.from(".flagship", {
     },
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Set the date we're counting down to
-    const countDownDate = new Date("Feb 16, 2024 00:00:00").getTime();
-    console.log(countDownDate);
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Set the date we're counting down to
+//     const countDownDate = new Date("Feb 16, 2024 00:00:00").getTime();
+//     console.log(countDownDate);
 
-    // Update the countdown every 1 second
-    const x = setInterval(function () {
-        // Get the current date and time
-        const now = new Date().getTime();
-        // console.log(now);
-        // Calculate the remaining time
-        const distance = countDownDate - now;
+//     // Update the countdown every 1 second
+//     const x = setInterval(function () {
+//         // Get the current date and time
+//         const now = new Date().getTime();
+//         // console.log(now);
+//         // Calculate the remaining time
+//         const distance = countDownDate - now;
 
-        // Calculate days, hours, minutes, and seconds
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//         // Calculate days, hours, minutes, and seconds
+//         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Display the countdown in the HTML
-        $("#days").html(days );
-        $("#hours").html(hours );
-        $("#minutes").html(minutes );
-        $("#seconds").html(seconds );
+//         // Display the countdown in the HTML
+//         $("#days").html(days );
+//         $("#hours").html(hours );
+//         $("#minutes").html(minutes );
+//         $("#seconds").html(seconds );
 
-        // If the countdown is over, display a message
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("countdown").innerHTML = "HORIZON HAS ARRIVED!!";
-        }
-    }, 1000);
-});
+//         // If the countdown is over, display a message
+//         if (distance < 0) {
+//             clearInterval(x);
+//             document.getElementById("countdown").innerHTML = "HORIZON HAS ARRIVED!!";
+//         }
+//     }, 1000);
+// });
 
 
 // ANimation for horizon page
